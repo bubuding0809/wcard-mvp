18ad28a8b78681d34acbb5e3d8a6a7d8b02fc62a
bubuding0809 <bubuding0809@gmail.com>
2022-09-11 13:15:40 +0800

    added custom theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,12 +4,27 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      // "light",
+      {
+        mytheme: {
+          primary: "#570DF8",
+          secondary: "#F000B8",
+          accent: "#37CDBE",
+          neutral: "#3D4451",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+          "base-100": "#FFFFFF",
+        },
+      },
+    ],
+  },
   plugins: [
     require("@tailwindcss/forms"),
     require("@tailwindcss/line-clamp"),
     require("daisyui"),
   ],
-  daisyui: {
-    themes: ["light"],
-  },
 };
