dca9fe15dffe9776e431a83b05cb43dac10b5014
bubuding0809 <bubuding0809@gmail.com>
2022-09-16 22:27:10 +0800

    gave spacer a list of size options

--- src/components/Spacer.tsx
+++ src/components/Spacer.tsx
@@ -1,5 +1,20 @@
-const Spacer: React.FC<{ size?: number }> = ({ size = 8 }) => {
-  return <div className={`p-${size}`}></div>;
+type SpacerProps = {
+  size?: "2xs" | "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl";
+};
+
+const SpacerSizes = {
+  "2xs": "0.5",
+  xs: "1",
+  sm: "2",
+  md: "4",
+  lg: "8",
+  xl: "10",
+  "2xl": "14",
+  "3xl": "16",
+};
+
+const Spacer: React.FC<SpacerProps> = ({ size = "sm" }) => {
+  return <div className={`p-${SpacerSizes[size]}`}></div>;
 };
 
 export default Spacer;
